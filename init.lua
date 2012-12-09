8dc5a68ebfb656d3310f8867b56826240cd19e1d
PilzAdam <adam-k@outlook.com>
2012-12-09 20:22:14 +0100

    Increase turning speed

--- init.lua
+++ init.lua
@@ -83,10 +83,10 @@ function boat:on_step(dtime)
 			self.v = self.v-0.08
 		end
 		if ctrl.left then
-			self.object:setyaw(self.object:getyaw()+math.pi/180)
+			self.object:setyaw(self.object:getyaw()+math.pi/120)
 		end
 		if ctrl.right then
-			self.object:setyaw(self.object:getyaw()-math.pi/180)
+			self.object:setyaw(self.object:getyaw()-math.pi/120)
 		end
 	end
 	local s = get_sign(self.v)
